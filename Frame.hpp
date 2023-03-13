6cbf76d4403387ab4f4fef7c4f31b812ed183c94
Vedant Nair <vedantxiii@gmail.com>
2023-03-13 07:01:41 +0530

    Float and Int conversion functions added

--- Frame.hpp
+++ Frame.hpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <sstream>
 
+int to_Int(std::string);
+
 class column{
     int x;
 };
@@ -24,11 +26,13 @@ std::vector<std::string> breakup(std::string s, char del){
 
 class WrongFileTypeException{
     public:
-        WrongFileTypeException(std::string name, std::string end){
-            name = name;
-            end = end;
+        WrongFileTypeException(std::string a, std::string b){
+            name = a;
+            end = b;
+            std::cout<<name<<"   "<<end<<std::endl; 
         }
         void printmsg(){
+            std::cout<<name<<"   "<<end<<std::endl; 
             std::cerr<<std::endl<<name<<" does not end with .csv";
             std::cerr<<std::endl<<"File ends with "<<end;
             std::cerr<<std::endl<<"Required File Type is comma separated value file (.csv)";
@@ -38,9 +42,25 @@ class WrongFileTypeException{
         std::string name,end;
 };
 
+class FileNotFoundException{
+    public:
+        FileNotFoundException(std::string a){
+            name = a;
+        }
+        void printmsg(){
+            std::cerr<<std::endl<<name<<" does not exist";
+            return;
+        }
+    private:
+        std::string name,end;
+};
+
+
+
 class Frame{
     public:
         column* read(std::string);
+        Frame(std::string);
         Frame(std::string,int);
     private:
         column* a;
@@ -50,35 +70,82 @@ class Frame{
 
 };
 
-Frame::Frame(std::string FILENAME, int awk = 0){
-    if(awk == 0){
-        try{
-            if(FILENAME.substr(FILENAME.length()-4,4) != ".csv"){
-                WrongFileTypeException w = WrongFileTypeException(FILENAME, FILENAME.substr(FILENAME.length()-4,4));
-                throw w;
-            }
-            std::ifstream fin;
-            if(fin.fail()){
-                std::cout<<"File Not Found";
-                std::cout<<"HIIIIIIIIIIIIIII";
-                return;
-            }
-            std::cout<<"BYRRRRRR";
-            std::string names;
-            getline(fin,names);
-            std::vector<std::string> c = breakup(names,',');
-            for(int i=0;i<c.size();i++){
-                std::cout<<c.at(i)<<std::endl;
-            }
-            dim[0] = c.size();
+Frame::Frame(std::string FILENAME){
+    try{
+        if(FILENAME.substr(FILENAME.length()-4,4) != ".csv"){
+            WrongFileTypeException w = WrongFileTypeException(FILENAME, FILENAME.substr(FILENAME.length()-4,4));
+            throw w;
         }
-        catch(WrongFileTypeException e){
-            e.printmsg();
+        std::ifstream fin;
+        fin.open(FILENAME);
+        if(fin.fail()){
+            FileNotFoundException f(FILENAME);
+            throw f;
         }
+        std::cout<<"BYRRRRRR";
+        std::string names;
+        getline(fin,names);
+        std::vector<std::string> c = breakup(names,',');
+        for(int i=0;i<c.size();i++){
+            std::cout<<c.at(i)<<std::endl;
+        }
+        dim[0] = c.size();
     }
-    else{
-        
+    catch(WrongFileTypeException e){
+        e.printmsg();
     }
+    catch(FileNotFoundException e){
+        e.printmsg();
+    }
+
+}
+
+int to_Int(std::string a){
+    int n = 0;
+    if(a.length()==0){
+        return 0;
+    }
+    if(a.at(0) == '-'){
+        for(int i=1;i<a.length();i++){
+            n = n*10 - (a.at(i)-48);
+        }
+        return n;
+    }
+    for(int i=0;i<a.length();i++){
+        n = n*10 + (a.at(i)-48);
+    }
+    return n;
+}
+
+double toFloat(std::string a){
+    double n = 0;
+    if(a.length()==0){
+        return 0;
+    }
+    int i = 0;
+    int l = a.length();
+    while(i < l){
+        if(a.at(i) == '.'){
+            break;
+        }
+        n = (a.at(0) == '-')?(n*10 - (a.at(i)-48)):(n*10 + (a.at(i)-48));
+        i++;
+    }
+    if(i == l){
+        return n;
+    }
+    double m = 0;
+    i++;
+    int f = i;
+    while(i != a.length()){
+        m = m*10 + (a.at(i)-48);
+        i++;
+    }
+    while(f < l){
+        m/=10.0;
+        f++;
+    }
+    return n+m;
 }
 
 #endif
