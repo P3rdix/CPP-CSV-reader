c15a5f65d67c25a236555f61a6df67d0464c0534
Vedant Nair <vedantxiii@gmail.com>
2023-03-15 02:32:29 +0530

    Column initialization function added. Testing required

--- Frame.hpp
+++ Frame.hpp
@@ -6,11 +6,16 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <stdlib.h>
 
 int to_Int(std::string);
 
-class column{
-    int x;
+struct column{
+    int*i;
+    float* f;
+    char** s;
+    std::string name;
+    struct column* next;
 };
 
 std::vector<std::string> breakup(std::string s, char del){
@@ -29,13 +34,15 @@ class Frame{
         Frame(std::string);
         Frame(std::string,int);
     private:
-        std::string* col_names;
+        ** col_names;
         int dim[2];
         int success;
+        struct column* x;
 };
 
 Frame::Frame(std::string FILENAME){
     try{
+        x = NULL;
         if(FILENAME.substr(FILENAME.length()-4,4) != ".csv"){
             throw FILENAME+"T";
         }
@@ -69,6 +76,14 @@ Frame::Frame(std::string FILENAME){
                 b[i].push_back(c.at(i));
             }
         }
+        dim[1] = b[0].size();
+        struct column* q=x;
+        for(int i=0;i<dim[0];i++){
+            struct column* t = new struct column;
+            if(x == NULL){
+                x = 
+            }
+        }
     }
     catch(std::string e){
         if(e == FILENAME+"T"){
@@ -81,6 +96,38 @@ Frame::Frame(std::string FILENAME){
         }
         success = -1;
     }
+}
+
+struct column* create_col(struct column* x, std::vector<std::string> y, int a, int dim){
+    if(a == 0){
+        x->i = new int(dim);
+        for(int j=0;j<dim;j++){
+            *(x->i + j) = std::stoi(y.at(j));
+        }
+        x->s = NULL;
+        x->f = NULL;
+    }
+    else if(a == 1){
+        x->f = new float(dim);
+        for(int j=0;j<dim;j++){
+            *(x->i + j) = std::stof(y.at(j));
+        }
+        x->i = NULL;
+        x->s = NULL;
+    }
+    else{
+        x->s = (char**)malloc(sizeof(char*)*dim);
+        for(int j=0;j<dim;j++){
+            *(x->s+j) = (char*)malloc(sizeof(char)*y.at(j).length());
+            for(int k=0;k<y.at(j).length();k++){
+                *(*(x->s+j)+k) = y.at(j).at(k);
+            }
+        }
+        x->i = NULL;
+        x->f = NULL;
+    }
+    x->next = NULL;
+    return x;
 }
 
 #endif
